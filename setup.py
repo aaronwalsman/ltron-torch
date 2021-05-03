b514245a074dec995d9ca3ba2b5f776ee1a49658
Aaron Walsman <aaronwalsman@gmail.com>
2021-05-03 09:38:52 -0700

    spatial_random_stack_redux

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ltron-torch",
+    version="0.0.0",
+    install_requires = ['ltron', 'tqdm', 'numpy', 'pyquaternion'],
+    author="Aaron Walsman",
+    author_email="aaronwalsman@gmail.com",
+    description='LTRON Torch Experiments"',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aaronwalsman/ltron-torch",
+    packages=setuptools.find_packages(),
+    scripts=[],
+)
