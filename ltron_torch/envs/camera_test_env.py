6c1df69b8388b5b83028595268b2a6abde574b69
Aaron Walsman <aaronwalsman@gmail.com>
2021-05-23 21:39:10 -0700

    updates'

---
+++ ltron_torch/envs/camera_test_env.py
@@ -0,0 +1,87 @@
+import math
+import collections
+
+from ltron.gym.ltron_env import LtronEnv
+from ltron.gym.components.scene import SceneComponent
+from ltron.gym.components.episode import MaxEpisodeLengthComponent
+from ltron.gym.components.dataset import DatasetPathComponent
+from ltron.gym.components.labels import InstanceListComponent
+from ltron.gym.components.spatial_info import InstancePoseComponent
+from ltron.gym.components.viewpoint import (
+    RandomizedAzimuthalViewpointComponent,
+    ControlledAzimuthalViewpointComponent,
+)
+from ltron.gym.components.colors import RandomizeColorsComponent
+from ltron.gym.components.visibility import PixelVisibilityComponent
+from ltron.gym.components.render import (
+    ColorRenderComponent, SegmentationRenderComponent)
+from ltron.gym.components.dense_map import DenseMapComponent
+
+def camera_test_env(
+    dataset,
+    split,
+    subset=None,
+    rank=0,
+    size=1,
+    width=256,
+    height=256,
+):
+    
+    components = collections.OrderedDict()
+    
+    # dataset
+    reset_mode='uniform'
+    components['dataset'] = DatasetPathComponent(
+        dataset,
+        split=split,
+        subset=subset,
+        rank=rank,
+        size=size,
+        reset_mode=reset_mode,
+        observe_episode_id=True,
+    )
+    dataset_info = components['dataset'].dataset_info
+    
+    # scene
+    components['scene'] = SceneComponent(
+        path_component=components['dataset'],
+        renderable=True,
+    )
+    
+    # max episode length
+    components['episode_length'] = MaxEpisodeLengthComponent(6)
+    
+    # viewpoint
+    components['viewpoint'] = ControlledAzimuthalViewpointComponent(
+        components['scene'],
+        azimuth_steps=12,
+        elevation_range=(math.radians(-60), math.radians(60)),
+        elevation_steps=5,
+        distance_range=(200,350),
+        distance_steps=4,
+        start_position='uniform',
+        observe_camera_parameters=False,
+        observe_camera_matrix=False,
+    )
+    
+    # color render
+    components['color_render'] = ColorRenderComponent(
+        width,
+        height,
+        components['scene'],
+        anti_alias=True,
+    )
+    
+    # instance labels
+    num_classes = max(dataset_info['class_ids'].values())+1
+    components['class_label'] = InstanceListComponent(
+        num_classes,
+        dataset_info['max_instances_per_scene'],
+        components['dataset'],
+        components['scene'],
+    )
+    
+    # build the env
+    env = LtronEnv(components, print_traceback=True)
+    
+    return env
---
+++ ltron_torch/envs/static_spatial_env.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import random
+import argparse
+import os
+
+import numpy
+
+from PIL import Image
+
+from ltron.gym.ltron_env import async_ltron
+from ltron_torch.envs.spatial_env import pose_estimation_env
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--output', type=str, default='.')
+parser.add_argument('--resolution', type=str, default='256x256')
+parser.add_argument('--label-resolution', type=str, default='32x32')
+parser.add_argument('--num-sequences', type=int, default=None)
+
+args = parser.parse_args()
+width, height = args.resolution.lower().split('x')
+width = int(width)
+height = int(height)
+label_width, label_height = args.label_resolution.lower().split('x')
+label_width = int(label_width)
+label_height = int(label_height)
+
+if __name__ == '__main__':
+    env = async_ltron(
+        1,
+        pose_estimation_env,
+        dataset='random_stack_redux',
+        split='all',
+        subset=args.num_sequences,
+        width=width,
+        height=height,
+        segmentation_width=label_width,
+        segmentation_height=label_height,
+        controlled_viewpoint=False,
+        train=False
+    )
+    
+    all_finished = False
+    
+    step_observations = env.reset()
+    while not all_finished:
+        actions = [{} for _ in range(env.num_envs)]
+        select_actions = numpy.zeros(
+            (env.num_envs, label_height, label_width),
+            dtype=numpy.bool)
+        episode_ids = step_observations['dataset']['episode_id']
+        episode_step = step_observations['episode_length']
+        for i, ep in enumerate(episode_ids):
+            if step_observations['scene']['valid_scene_loaded'][i]:
+                print('saving episode: %i frame %i'%(ep, episode_step[i]))
+                image = Image.fromarray(step_observations['color_render'][i])
+                color_path = os.path.join(
+                    args.output,
+                    'color_%i_%i.png'%(ep, episode_step[i]),
+                )
+                image.save(color_path)
+                
+                label_path = os.path.join(
+                    args.output,
+                    'label_%i_%i.npy'%(ep, episode_step[i]),
+                )
+                numpy.save(
+                    label_path,
+                    step_observations['dense_class_labels'][i]
+                )
+                
+                pose_path = os.path.join(
+                    args.output,
+                    'pose_%i_%i.npy'%(ep, episode_step[i]),
+                )
+                numpy.save(pose_path, step_observations['dense_pose_labels'][i])
+                
+                foreground = (
+                    step_observations['dense_class_labels'][i,:,:,0] != 0
+                )
+                ys, xs = numpy.where(foreground)
+                selected_pixel = random.randint(0, ys.shape[0]-1)
+                y = ys[selected_pixel]
+                x = xs[selected_pixel]
+                select_actions[i][y,x] = 1
+                actions[i]['visibility'] = select_actions[i]
+        
+        (step_observations,
+         step_rewards,
+         step_terminal,
+         step_info) = env.step(actions)
+        
+        all_finished = (
+            not numpy.any(step_observations['scene']['valid_scene_loaded']))
