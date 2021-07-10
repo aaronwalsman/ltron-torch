d9a1a23dd2d6c2b592c042c97858e9d4a23b10e4
Aaron Walsman <aaronwalsman@gmail.com>
2021-07-09 21:33:29 -0700

    transformer stuuuuuuff

--- ltron_torch/models/transformer.py
+++ ltron_torch/models/transformer.py
@@ -1,11 +1,15 @@
 import torch
 from torch.nn import (
-    Module, Sequential, Linear, Dropout, ReLU, GELU, MultiheadAttention)
+    Module, Sequential, Linear, Dropout, ReLU, GELU, MultiheadAttention,
+    Embedding, LayerNorm, TransformerEncoderLayer, Parameter
+)
 import torch.nn.functional as F
 
-from ltron_torch.models.multihead_attention import SparseMultiheadAttention
-import ltron_torch.models.transformer_masks as transformer_masks
+from ltron_torch.models.multihead_attention import (
+    SparseMultiheadAttention, FixedMaskMultiheadAttention,
+)
 from ltron_torch.models.positional_encoding import positional_encoding
+import ltron_torch.models.transformer_masks as transformer_masks
 
 class TransformerConfig:
     vocabulary = 4096
@@ -18,20 +22,27 @@ class TransformerConfig:
     mask = 'full'
     
     block = 'gpt'
-    attention_module = 'sparse'
+    attention_module = 'torch'
     nonlinearity = 'gelu'
     
-    channels = 768
     num_layers = 12
+    channels = 768
+    residual_channels = None
     num_heads = 12
-    output_channels = 1
+    decoder_channels = 1
+    
+    learned_positional_encoding = False
+    # so far, for most purposes, multi is worse than single
+    positional_encoding_dimensions = 'single' # single/multi
     
     embedding_dropout = 0.1
     residual_dropout = 0.1
     attention_dropout = 0.1
     
+    init_weights = False
+    
     def __init__(self, **kwargs):
-        for key, value in kwargs:
+        for key, value in kwargs.items():
             assert hasattr(self, key)
             setattr(self, key, value)
 
@@ -40,20 +51,33 @@ def make_attention_mask(config):
         tokens = (
             config.map_height *
             config.map_width *
-            sequence_length +
+            config.sequence_length +
             config.decoder_tokens
         )
         return transformer_masks.full(tokens)
+    elif config.mask == 'blowfish':
+        return transformer_masks.blowfish(
+            config.decoder_tokens,
+            config.map_height * config.map_width,
+            config.sequence_length,
+        )
 
 def make_attention_module(config):
+    mask = make_attention_mask(config)
     if config.attention_module == 'sparse':
-        return SparseMultiheadAttention(something)
+        sparsity_pattern = torch.nonzero(mask, as_tuple=True)
+        return SparseMultiheadAttention(
+            sparsity_pattern,
+            config.channels,
+            config.num_heads,
+            config.attention_dropout,
+            config.residual_dropout,
+        )
     
     elif config.attention_module == 'mingpt':
         return MinGPTMultiheadAttention(something)
     
     elif config.attention_module == 'torch':
-        mask = make_attention_mask(config)
         return FixedMaskMultiheadAttention(
             mask, config.channels, config.num_heads, config.attention_dropout)
 
@@ -68,41 +92,106 @@ def make_block(config):
         return TransformerBlock(config)
     elif config.block == 'gpt':
         return GPTBlock(config)
+    elif config.block == 'torch':
+        residual_channels = config.residual_channels
+        if residual_channels is None:
+            residual_channels = 4 * config.channels
+        return TransformerEncoderLayer(
+            config.channels,
+            config.num_heads,
+            residual_channels,
+            config.residual_dropout,
+        )
 
 class EmbeddingBlock(Module):
     def __init__(self, config):
-        super(GPTEmbeddingBlock, self).__init__()
+        super(EmbeddingBlock, self).__init__()
         
         self.embedding = Embedding(config.vocabulary, config.channels)
+        
+        self.decoder_tokens = config.decoder_tokens
+        self.decoder_embedding = Embedding(self.decoder_tokens, config.channels)
+        
         # TODO: make the positional encoding happen conditionally when/if we
         # try the Transformer-XL thing.
-        self.positional_encoding = positional_encoding(
-            config.channels,
-            (config.sequence_length, config.map_height * config.map_width),
-        )
+        
+        if config.learned_positional_encoding:
+            if config.positional_encoding_dimensions == 'multi':
+                p = torch.zeros(
+                    config.sequence_length,
+                    config.map_height * config.map_width,
+                    1,
+                    config.channels)
+            elif config.positional_encoding_dimensions == 'single':
+                num_tokens = (
+                    config.sequence_length *
+                    config.map_height *
+                    config.map_width
+                )
+                p = torch.zeros(num_tokens, 1, config.channels)
+                    
+            else:
+                raise NotImplementedError
+            
+            self.positional_encoding = Parameter(p)
+        else:
+            if config.positional_encoding_dimensions == 'multi':
+                p = positional_encoding(
+                    config.channels,
+                    config.sequence_length,
+                    config.map_height * config.map_width,
+                ).unsqueeze(2)
+            elif config.positional_encoding_dimensions == 'single':
+                num_tokens = (
+                    config.sequence_length *
+                    config.map_height *
+                    config.map_width
+                )
+                p = positional_encoding(
+                    config.channels, num_tokens).unsqueeze(1)
+            else:
+                raise NotImplementedError
+            
+            self.register_buffer('positional_encoding', p)
+        
         self.dropout = Dropout(config.embedding_dropout)
     
     def forward(self, x):
         x = self.embedding(x)
-        x = x + self.positional_encoding
+        s, hw, b, c = x.shape
+        x = x + self.positional_encoding.view(s, hw, 1, c)
+        x = x.view(s*hw, b, c)
+        
+        d = self.decoder_embedding.weight.view(self.decoder_tokens, 1, c)
+        #d = d + self.p2[:self.decoder_tokens]
+        #d = d.view(self.decoder_tokens, 1, c)
+        d = d.expand(self.decoder_tokens, b, c)
+        x = torch.cat((d, x), dim=0)
+        #s, b, c = x.shape
+        #x = x + self.p2[:s]
+        
         x = self.dropout(x)
         
         return x
 
 class TransformerBlock(Module):
     def __init__(self, config):
-        super(TransformerEncoderLayer, self).__init__()
+        super(TransformerBlock, self).__init__()
+        residual_channels = config.residual_channels
+        if residual_channels is None:
+            residual_channels = config.channels*4
+        
         self.attention_residual = Sequential(
             make_attention_module(config),
-            Dropout(config),
+            Dropout(config.residual_dropout),
         )
         self.attention_norm = LayerNorm(config.channels)
         
         self.projection_residual = Sequential(
-            Linear(config.channels, 4*config.channels),
+            Linear(config.channels, residual_channels),
             make_nonlinearity(config),
             Dropout(config.residual_dropout),
-            Linear(4*config.channels, config.channels),
+            Linear(residual_channels, config.channels),
             Dropout(config.residual_dropout),
         )
         self.residual_norm = LayerNorm(config.channels)
@@ -112,24 +201,33 @@ class TransformerBlock(Module):
         x = self.attention_norm(x)
         x = x + self.projection_residual(x)
         x = self.residual_norm(x)
+        
+        return x
 
 class GPTBlock(Module):
     def __init__(self, config):
+        super(GPTBlock, self).__init__()
+        residual_channels = config.residual_channels
+        if residual_channels is None:
+            residual_channels = config.channels*4
+        
         self.attention_residual = Sequential(
             LayerNorm(config.channels),
             make_attention_module(config),
         )
         self.projection_residual = Sequential(
             LayerNorm(config.channels),
-            Linear(config.channels, 4*config.channels),
+            Linear(config.channels, residual_channels),
             make_nonlinearity(config),
-            Linear(4*config.channels, config.channels),
+            Linear(residual_channels, config.channels),
             Dropout(config.residual_dropout),
         )
     
     def forward(self, x):
-        x = x + attention_residual(x)
-        x = x + projection_residual(x)
+        x = x + self.attention_residual(x)
+        x = x + self.projection_residual(x)
+        
+        return x
 
 # TODO: Gated block from Stabilizing Transformers For Reinforcement Learning
 
@@ -140,8 +238,8 @@ class ReadHead(Module):
         self.decode_input = config.decode_input
         
         self.head = torch.nn.Sequential(
-            LayerNorm(config.channels),
-            Linear(config.channels, config.output_channels),
+            #LayerNorm(config.channels),
+            Linear(config.channels, config.decoder_channels),
         )
     
     def forward(self, x):
@@ -154,14 +252,31 @@ class ReadHead(Module):
 
 class TokenMapSequenceEncoder(Module):
     def __init__(self, config):
+        super(TokenMapSequenceEncoder, self).__init__()
         
         self.embedding = EmbeddingBlock(config)
         self.blocks = Sequential(
             *[make_block(config) for _ in range(config.num_layers)])
         
-        self.read_head = ReadHand(config.decoder_tokens, config.read_from)
+        self.read_head = ReadHead(config)
+        
+        # this is so far bad for me, but all my other parameters are not dialed
+        # in yet either, so...
+        if config.init_weights:
+            self.apply(self._init_weights)
         
     def forward(self, x):
         x = self.embedding(x)
         x = self.blocks(x)
-        x = self.layernorm(x)
+        x = self.read_head(x)
+        
+        return x
+    
+    def _init_weights(self, module):
+        if isinstance(module, (Linear, Embedding)):
+            module.weight.data.normal_(mean=0., std=0.02)
+            if isinstance(module, Linear) and module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.)
--- ltron_torch/models/transformer_masks.py
+++ ltron_torch/models/transformer_masks.py
@@ -3,10 +3,10 @@ import math
 import torch
 import torch.nn.functional as F
 
-def stingray(n):
-    return torch.tril(torch.ones(n, n, dtype=torch.bool), diagonal=-1)
+def full(n):
+    return torch.zeros((n, n), dtype=torch.bool)
 
-def neighbor(h, w, width=1):
+def neighborhood(h, w, width=1):
     mask = torch.ones((h,w,h,w), dtype=torch.bool)
     for y in range(h):
         for yy in range(y-width, y+width+1):
@@ -22,6 +22,9 @@ def neighbor(h, w, width=1):
     mask = mask.reshape(h*w, h*w)
     
     return mask
+
+def causal(n):
+    return torch.tril(torch.ones(n, n, dtype=torch.bool), diagonal=-1)
 
 def blowfish(h, n, s):
     '''
@@ -52,7 +55,7 @@ def octopus(h, n, s):
 
     return mask
 
-def anenome(n, s):
+def anemone(n, s):
     num_tokens = n * (1+s)
     mask = ~torch.eye(num_tokens, dtype=torch.bool)
     mask[:n, :n] = False
@@ -77,81 +80,3 @@ def jellyfish(n, s):
             mask[i*n:(i+1)*n, j*n:(j+1)*n] = block
 
     return mask
-
-# adapted from Karpathy mingpt for reference
-def karpathy_attn(q,k,v,n_head, mask):
-        
-        T,B,C = q.shape
-        q = q.permute(1,0,2)
-        k = k.permute(1,0,2)
-        v = v.permute(1,0,2)
-        
-        h,w = mask.shape
-        mask = mask.view(1,1,h,w)
-        
-        # calculate query, key, values for all heads in batch
-        # and move head forward to be the batch dim
-        # (B, nh, T, hs)
-        k = k.view(B, T, n_head, C // n_head).transpose(1, 2)
-        # (B, nh, T, hs)
-        q = q.view(B, T, n_head, C // n_head).transpose(1, 2)
-        # (B, nh, T, hs)
-        v = v.view(B, T, n_head, C // n_head).transpose(1, 2)
-
-        # causal self-attention;
-        # Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(mask[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        #att = self.attn_drop(att)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        # re-assemble all head outputs side by side
-        y = y.transpose(1, 2).contiguous().view(B, T, C)
-        # output projection
-        #y = self.resid_drop(self.proj(y))
-        return y.permute(1,0,2)
-
-def sparse_multihead_attention(q, k, v, n_heads, mask):
-    
-    #qk^T/d**0.5
-    s, b, c = v.shape
-    cc = c // n_heads
-    q = q.view(s, b, n_heads, cc)
-    k = k.view(s, b, n_heads, cc)
-    v = v.view(s, b, n_heads, cc)
-    ki, qi = torch.nonzero(mask, as_tuple=True)
-    qk = torch.einsum('sbhc,sbhc->sbh', q[qi], k[ki]).unsqueeze(-1)
-    qk = qk / (cc**0.5)
-    
-    # limited-stability softmax
-    # rather than stabilizing using the max of each sum, we stabilize with
-    # the max over all sums
-    max_qk = torch.max(qk)
-    eqk = torch.exp(qk - max_qk)
-    d = torch.zeros(s, b, n_heads, 1)
-    d.index_add_(0, qi, eqk)
-    
-    qkv = eqk * v[ki]
-    y = torch.zeros(s, b, n_heads, cc)
-    y.index_add_(0, qi, qkv)
-    y = y / d
-    
-    y = y.view(s, b, c)
-    
-    return y
-
-def test():
-    k = torch.rand(16,4,256) * 2. - 1.
-    q = torch.rand(16,4,256) * 2. - 1.
-    v = torch.rand(16,4,256) * 2. - 1.
-    
-    mask = ~jellyfish(4, 3)
-    #mask = torch.ones((3,3), dtype=torch.bool)
-    
-    x1 = sparse_multihead_attention(q, k, v, 4, mask)
-    x2 = karpathy_attn(q, k, v, 4, mask)
-    
-    print(torch.allclose(x1,x2, rtol=1e-3))   
-
-if __name__ == '__main__':
-    test()
