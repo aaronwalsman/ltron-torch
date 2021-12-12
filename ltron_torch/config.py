3ee189abf90e6fcdd32a46addf82011e8fea2147
Aaron Walsman <aaronwalsman@gmail.com>
2021-12-11 23:21:52 -0800

    trying configs with inheritance

--- ltron_torch/config.py
+++ ltron_torch/config.py
@@ -6,38 +6,41 @@ class Config:
         for key, value in kwargs.items():
             assert hasattr(self, key), 'Invalid Config Argument: %s'%key
             setattr(self, key, value)
-        self.set_dependent_variables()
+        
+        for Class in self.__class__.mro():
+            if isinstance(Class, Config):
+                super(Class, self).set_dependents()
         
         self.kwargs = kwargs
     
-    def set_dependent_variables(self):
+    def set_dependents(self):
         pass
     
     @classmethod
     def load_config(cls, file_path, section='CONFIG'):
         args = {}
-        config = configparser.ConfigParser()
-        config.read_file(open(os.path.expanduser(file_path)))
-        for name in config[section]:
+        parser = configparser.ConfigParser()
+        parser.read_file(open(os.path.expanduser(file_path)))
+        for name in parser[section]:
             try:
-                args[name] = config[section].getint(name)
+                args[name] = parser[section].getint(name)
                 continue
             except ValueError:
                 pass
             
             try:
-                args[name] = config[section].getboolean(name)
+                args[name] = parser[section].getboolean(name)
                 continue
             except ValueError:
                 pass
             
             try:
-                args[name] = config[section].getfloat(name)
+                args[name] = parser[section].getfloat(name)
                 continue
             except ValueError:
                 pass
             
-            value_string = config[section][name]
+            value_string = parser[section][name]
             if ',' in value_string:
                 for remove in '[]()':
                     value_string = value_string.replace(remove, '')
@@ -61,12 +64,137 @@ class Config:
     
     def write_config(self, file_path, section='CONFIG'):
         file_path = os.path.expanduser(file_path)
-        config = configparser.ConfigParser()
+        parser = configparser.ConfigParser()
         try:
-            config.read_file(open(file_path))
+            parser.read_file(open(file_path))
         except FileNotFoundError:
             pass
-        config[section] = self.kwargs
-        print('look at me go!')
+        parser[section] = self.kwargs
         with open(file_path, 'w') as f:
-            config.write(f)
+            parser.write(f)
+
+class OldConfig:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            assert hasattr(self, key), 'Invalid Config Argument: %s'%key
+            setattr(self, key, value)
+        self.set_dependents()
+        self.kwargs = kwargs
+    
+    def set_dependents(self):
+        pass
+    
+    @classmethod
+    def load_config(cls, file_path, section='CONFIG'):
+        args = {}
+        parser = configparser.ConfigParser()
+        parser.read_file(open(os.path.expanduser(file_path)))
+        for name in parser[section]:
+            try:
+                args[name] = parser[section].getint(name)
+                continue
+            except ValueError:
+                pass
+            
+            try:
+                args[name] = parser[section].getboolean(name)
+                continue
+            except ValueError:
+                pass
+            
+            try:
+                args[name] = parser[section].getfloat(name)
+                continue
+            except ValueError:
+                pass
+            
+            value_string = parser[section][name]
+            if ',' in value_string:
+                for remove in '[]()':
+                    value_string = value_string.replace(remove, '')
+                def convert_value(value):
+                    try:
+                        v = float(value)
+                        if v.is_integer():
+                            return int(v)
+                        else:
+                            return v
+                    except ValueError:
+                        return value
+                values = tuple(
+                    convert_value(v) for v in value_string.split(','))
+                args[name] = values
+                continue
+            
+            args[name] = value_string
+        
+        return cls(**args)
+    
+    def write_config(self, file_path, section='CONFIG'):
+        file_path = os.path.expanduser(file_path)
+        parser = configparser.ConfigParser()
+        try:
+            parser.read_file(open(file_path))
+        except FileNotFoundError:
+            pass
+        parser[section] = self.kwargs
+        with open(file_path, 'w') as f:
+            parser.write(f)
+
+class CompositeConfig(Config):
+    MemberClasses = ()
+    def __init__(self, **kwargs):
+        member_args = [{} for MemberClass in self.MemberClasses]
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+                found_key = True
+            else:
+                found_key = False
+            for args, MemberClass in zip(member_args, self.MemberClasses):
+                if hasattr(MemberClass, key):
+                    args[key] = value
+                    found_key = True
+            assert found_key, 'Invalid Config Argument: %s'%key
+        self.set_dependents()
+        self.kwargs = kwargs
+        
+
+def CompositeConfig(ConfigClasses):
+    class CustomCompositeConfig(Config):
+        def __init__(self, **kwargs):
+            #self.configs = [ConfigClass() for ConfigClass in ConfigClasses]
+            config_args = [{} for ConfigClass in ConfigClasses]
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+                    found_key = True
+                else:
+                    found_key = False
+                for ConfigClass in ConfigClasses:
+                    if hasattr(config, key):
+                        setattr(config, key, value)
+                        found_key = True
+                assert found_key, 'Invalid Config Argument: %s'%key
+            self.set_dependents()
+            self.kwargs = kwargs
+        
+        def __getattr__(self, attr):
+            for config in self.configs:
+                if hasattr(config, attr):
+                    return getattr(config, attr)
+            
+            raise AttributeError(attr)
+        
+        def __setattr__(self, attr, value):
+            for config in self.configs:
+                if hasattr(config, attr):
+                    setattr(config, attr, value)
+            
+            raise AttributeError(attr)
+        
+        def set_dependents(self):
+            for config in self.configs:
+                config.set_dependents()
+    
+    return CustomCompositeConfig
