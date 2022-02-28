74e9218f385735e6bf412f2fa62ba5e312b25bba
Aaron Walsman <awalsman@cs.washington.edu>
2022-02-27 21:27:38 -0800

    using zip splits now

--- ltron_torch/dataset/episode_dataset.py
+++ ltron_torch/dataset/episode_dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 from gym.vector.async_vector_env import AsyncVectorEnv
 
 from ltron.config import Config
-from ltron.dataset.paths import get_dataset_paths
+from ltron.dataset.paths import get_dataset_paths, get_zip_paths
 from ltron.hierarchy import index_hierarchy
 
 from ltron_torch.dataset.collate import pad_stack_collate
@@ -23,16 +23,31 @@ class EpisodeDatasetConfig(Config):
 class EpisodeDataset(Dataset):
     def __init__(self, config):
         self.config = config
-        paths = get_dataset_paths(
+        #paths = get_dataset_paths(
+        #    config.dataset, config.split, subset=config.subset)
+        #self.episode_paths = paths['episodes']
+        self.zipfile = None
+        _, self.names = get_zip_paths(
             config.dataset, config.split, subset=config.subset)
-        self.episode_paths = paths['episodes']
     
     def __len__(self):
-        return len(self.episode_paths)
+        #return len(self.episode_paths)
+        return len(self.names)
     
     def __getitem__(self, i):
-        path = self.episode_paths[i]
-        data = numpy.load(path, allow_pickle=True)['episode'].item()
+        #path = self.episode_paths[i]
+        #data = numpy.load(path, allow_pickle=True)['episode'].item()
+        
+        if self.zipfile is None:
+            self.zipfile, _ = get_zip_paths(
+                self.config.dataset,
+                self.config.split,
+                subset=self.config.subset
+            )
+        
+        name = self.names[i]
+        data = numpy.load(self.zipfile.open(name), allow_pickle=True)
+        data = data['episode'].item()
         
         return data
 
